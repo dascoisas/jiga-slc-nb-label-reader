35cbfa5e036c04ace4fccf3b423da94a5143ea39
BrunaDev <dev.bruna.magalhaes@gmail.com>
2024-09-30 14:55:41 -0300

    Revert ":construction: adding jiga slc nb"
    This reverts commit a128462d5541ad5ee4dbc42e816802b9143e4325.

--- src/serverURL.js
+++
@@ -1,9 +0,0 @@
-let baseURL = null;
-
-if (process.env.REACT_APP_ENV === 'DEV') {
-  baseURL = `http://${window.location.hostname}:8000`;
-} else {
-  baseURL = process.env.REACT_APP_NLM_SERVER_DNS;
-}
-
-export default baseURL;
